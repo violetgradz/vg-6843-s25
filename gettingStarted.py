e573b1b1f701c2be94009ffbbe80b5aa528a4e5f
Beatriz D <126616469+dat-angel@users.noreply.github.com>
2025-02-15 14:25:29 -0800

    remove extra space

--- gettingStarted.py
+++ gettingStarted.py
@@ -18,10 +18,11 @@ def welcome_assignment_answers(question):
         answer = "a6e90ac75f64f27949bb760b75bfa797e586c990c31905f5f23ae8ab904d0068"
     elif question =="Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
-    elif question =="What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+    elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
         answer = 3
-    elif question =="What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+    elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
         answer = 3
+
     else:
         answer = "Question not recognized. Please check for typos."
     
