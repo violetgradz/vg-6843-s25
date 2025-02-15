8a07db7359d2c935d2cf51c1f41c4c89c4d913de
Beatriz D <126616469+dat-angel@users.noreply.github.com>
2025-02-15 14:29:54 -0800

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -4,9 +4,10 @@
 
 #Questions:
 def welcome_assignment_answers(question):
-    if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+    print(f"Received Question: {question}")  # Debugging print
+    if question =="In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
         answer = "pcap"
-    elif question == "Are encoding and encryption the same? - Yes/No":
+    elif question =="Are encoding and encryption the same? - Yes/No":
         answer = "No"  
     elif question =="Is it possible to decrypt a message without a key? - Yes/No":
         answer = "No"
@@ -18,10 +19,10 @@ def welcome_assignment_answers(question):
         answer = "a6e90ac75f64f27949bb760b75bfa797e586c990c31905f5f23ae8ab904d0068"
     elif question =="Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
-    elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = 3
-    elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-        answer = 3
+    elif question =="What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+        answer = "3"
+    elif question =="What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+        answer = "3"
 
     else:
         answer = "Question not recognized. Please check for typos."
