4c7cc2be88cbb30dc5abc33fef4a9631f964fd70
Beatriz D <126616469+dat-angel@users.noreply.github.com>
2025-02-15 14:36:33 -0800

    integer changes

--- ### welcome_assignment_answers.py
+++ ### welcome_assignment_answers.py
@@ -4,26 +4,24 @@
 
 #Questions:
 def welcome_assignment_answers(question):
-    print(f"Received Question: {question}")  # Debugging print
-    if question =="In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+    if question == "Are encoding and encryption the same? - Yes/No":
+        answer = "No"
+    elif question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
         answer = "pcap"
-    elif question =="Are encoding and encryption the same? - Yes/No":
-        answer = "No"  
     elif question =="Is it possible to decrypt a message without a key? - Yes/No":
         answer = "No"
     elif question =="Is it possible to decode a message without a key? - Yes/No":
         answer = "Yes"
     elif question =="Is a hashed message supposed to be un-hashed? - Yes/No":
         answer = "No"
-    elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
+    elif question =="What is the SHA256 hashing value of your NYU email and use the answer in your code":
         answer = "a6e90ac75f64f27949bb760b75bfa797e586c990c31905f5f23ae8ab904d0068"
     elif question =="Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
-    elif question =="What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = "5"
-    elif question =="What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-        answer = "2"
-
+    elif question =="What layer of the TCP/IP model does the protocol DNS belong to?":
+        answer = 3
+    elif question =="What layer of the TCP/IP model does the protocol ICMP belong to?":
+        answer = 3
     else:
         answer = "Question not recognized. Please check for typos."
     
